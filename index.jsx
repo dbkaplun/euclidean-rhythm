b6e30c3f006dc5632748ce002f2e529035055aad
Dan Kaplun <dbkaplun@gmail.com>
2015-11-12 19:20:53 -0500

    feat(play): make it work, add wikipedia quote

--- index.jsx
+++ index.jsx
@@ -8,11 +8,28 @@ var EuclideanRhythmDemo = React.createClass({
     return {
       onNotes: 3,
       totalNotes: 8,
-      state: 'stop'
+      state: 'stop',
+      rhythmIndex: 0,
+
+      // MIDI options
+      volume: 127,
+      channel: 0,
+      note: 50,
+      velocity: 127,
+      duration: .25
     };
   },
   componentDidMount: function () {
-    // TODO: init MIDI.js
+    var self = this;
+  	MIDI.loadPlugin({
+  		soundfontUrl: "node_modules/midi/examples/soundfont/",
+  		instrument: "acoustic_grand_piano",
+  		onprogress: console.log.bind(console),
+  		onsuccess: function() {
+        MIDI.setVolume(self.state.channel, self.state.volume);
+        self.setState({midi: MIDI});
+  		}
+  	});
   },
   getRhythm: function () {
     return euclideanRhythm(this.state.onNotes, this.state.totalNotes)
@@ -23,22 +40,43 @@ var EuclideanRhythmDemo = React.createClass({
       play: 'stop'
     }[this.state.state];
   },
+  play: function () {
+    if (this.getRhythm()[this.state.rhythmIndex]) {
+      MIDI.noteOn(this.state.channel, this.state.note, this.state.velocity, 0);
+      MIDI.noteOff(this.state.channel, this.state.note, this.state.duration);
+    }
+    this.render();
+    this.setState({
+      state: 'play',
+      rhythmIndex: (this.state.rhythmIndex + 1) % this.state.totalNotes,
+      timeout: setTimeout(this.play, this.state.duration * 1000)
+    });
+  },
+  stop: function () {
+    MIDI.noteOff(this.state.channel, this.state.note, 0);
+    clearTimeout(this.state.timeout);
+    this.setState({
+      state: 'stop',
+      rhythmIndex: 0,
+      timeout: null
+    });
+  },
+  toggle: function (evt) {
+    this[this.getNextState()]();
+  },
   onChange: function (evt) {
     var newState = {};
     newState[evt.target.name] = Number(evt.target.value);
     this.setState(newState);
-    if (this.state.state !== 'stop') this.toggle();
-  },
-  toggle: function (evt) {
-    // TODO: MIDI.js
-    this.setState({
-      state: this.getNextState()
-    });
+    this.stop();
   },
   renderRhythm: function () {
-    return this.getRhythm()
-      .map(function (beat) { return beat ? 'x' : '-'; })
-      .join('');
+    var self = this;
+    return self.getRhythm().map(function (beat, i) {
+      var renderedBeat = beat ? 'x' : '-';
+      if (self.state.state === 'play' && self.state.rhythmIndex === i) renderedBeat = <strong key={i}>{renderedBeat}</strong>;
+      return renderedBeat;
+    });
   },
   render: function () {
     return (
